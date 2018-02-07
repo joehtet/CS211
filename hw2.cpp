9e6549f19edcef1a82ff402778b6c1f47ecca01e
Joseph Htet <joehtet@gmail.com>
2018-02-06 23:47:32 -0500

    1st commit

--- hw2.cpp
+++ hw2.cpp
@@ -4,7 +4,36 @@
 #include <iostream>
 using namespace std;
 
-bool equivalent(int a[], int b[], int n);
+static bool isEqual(int a1[], int a2[], int n) {
+
+    for(int i=0; i<n; i++) {
+//        cout << a1[i] << " " << a2[i] << endl;
+        if(a1[i]!=a2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool equivalent(int a[], int b[], int n) {
+    int tempArray[n];
+
+    for(int i=0; i<n; i++) {
+
+        for(int j=0; j<n; j++) {
+            tempArray[j] = b[(i+j)%n];
+        }
+
+        if(isEqual(a, tempArray, n)) {
+            return true;    
+        };
+
+    }
+
+    return false;    
+};
 
 int main() {
    cout << boolalpha; // to show boolean value as "true" or "false"
@@ -21,6 +50,9 @@ int main() {
    int a6[4] = {1, 0, 1, 1};
    cout << equivalent(a5, a6, 4) << endl; // true
 
+   int a7[4] = {5, 6, 7, 8};
+   int a8[4] = {5, 6, 7, 8};
+   cout << equivalent(a7, a8, 4) << endl; // true
 
    return 0;
 }
