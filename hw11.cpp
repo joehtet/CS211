bae86f9705043fa52660c0d0d5a7feaece7cbaff
Joseph Htet <joehtet@gmail.com>
2018-03-16 18:18:27 -0400

    done hw11

--- hw11.cpp
+++ hw11.cpp
@@ -1,22 +1,89 @@
+
+// Htet, Joseph
+// CS211 Section 22D Homework 9
+
+/*
+    Number of solutions to the n-queen problem.
+*/
+
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
 using namespace std;
 
-
-int getSolutions(int n) {
-// Return number of solutions to the n queen problem.
-    return 0;
+void backtrack(int &c) {
+    c--;
 }
+
+bool ok(int q[], int c) {
+    for(int i = 0; i < c; i++) {
+        if(q[i] == q[c] || (c-i) == abs(q[c] - q[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int getNumSolutions(int n) {
+    int *q = new int[n];
+    int counter = 0; 
+
+    // initialize    
+    int c = 0;
+    int row=0;
+    q[0] = 0;
+    bool fromBacktrack = false;
+
+
+    while(true) {
+    // loop through columns
+        while(c!=n) {
+
+            if(fromBacktrack) {
+                row = q[c]+1;
+                fromBacktrack = false;
+            }
+            else { row=0; }
+            
+            for(q[c]=row; q[c]!=n; ++q[c]) {
+                if(ok(q, c)) {
+                    break;
+                }
+            }
+
+            if(q[c] >= n) {
+                backtrack(c);
+                if(c==-1) { 
+                    delete [] q;
+                    return counter;
+                }
+                fromBacktrack = true;
+            }
+
+            else { c++; }
+
+        }
+
+        counter++;
+        backtrack(c);
+        if(c==-1) { 
+            delete [] q;
+            return counter;
+        }
+        fromBacktrack = true;
+    }
+}
+
 
 int main() {
     
     int n;
-    cout << "Enter number of queens: ";
+    cout << "Enter number of queens: " << endl;
     cin >> n;
 
-    for(int i=0; i<n; i++) {
-        cout << i+1 << ". There are " <<    getSolutions(n) << " solutions to the " << n << " queens problem." << endl;;
+    for(int i=1; i<=n; i++) {
+        cout << i << ". There are " <<    getNumSolutions(i) << " solutions to the " << i << " queens problem." << endl;;
     }
- 
 
     return 0;
 }
